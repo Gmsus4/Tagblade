08b7dbab72d0b88f591cdf3f00f031d953ec2923
Fernando <fernando452000@gmail.com>
2025-04-21 15:09:21 -0600

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: 'class', // usamos la clase "dark" para activar el modo oscuro
+    content: ['./src/**/*.{html,js,astro}'], // ajusta según tu estructura
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
