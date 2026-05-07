725b670cb978e01c14c10a979a8e71a5df9b1e55
Fernando Gómez <fernando452000@gmail.com>
2026-05-06 19:45:15 -0600

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://tagblade.com",
   vite: {
     plugins: [...tailwind()],
   },
+
+  integrations: [sitemap()],
 });
